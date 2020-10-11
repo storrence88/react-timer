e97acc961337b141264a00fa673fd71e1c9b4d4f
Steven Torrence <storrence88@gmail.com>
2020-10-11 17:31:59 -0500

    Added styled components to the application

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  border-radius: 5px;
+  background: orange;
+  color: white;
+  border: solid 1px white;
+  text-decoration: none;
+  cursor: pointer;
+  font-size: 1.2rem;
+  padding: 18px 10px;
+  width: 180px;
+  margin: 10px;
+  outline: none;
+  &:hover {
+    transition: all 0.5s ease-in-out;
+    background: white;
+    border: solid 1px white;
+    color: orange;
+  }
+`;
 
 const Button = (props) => {
-  return <button onClick={props.action}>{props.buttonName}</button>;
+  return <StyledButton onClick={props.action}>{props.buttonName}</StyledButton>;
 };
 
 export default Button;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Title = styled.h2`
+  text-transform: uppercase;
+  font-size: 2.6rem;
+  font-weight: 100;s
+`;
 
 const Header = () => {
-  return (
-    <div>
-      <h2>React Stopwatch</h2>
-    </div>
-  );
+  return <Title>React Stopwatch</Title>;
 };
 
 export default Header;
